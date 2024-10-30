4cca54fb8eee990939058ac7314ca71d40e37685
Vladimirskiy Aleksandr <alexvl89@yandex.ru>
2024-10-30 22:33:09 +0300

    Инициализация

---
+++ KafkaConsumer/Program.cs
@@ -0,0 +1,41 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        var config = new ConsumerConfig
+        {
+            GroupId = "transaction-group",
+            BootstrapServers = "localhost:9092",
+            AutoOffsetReset = AutoOffsetReset.Earliest
+        };
+
+        using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+        {
+            consumer.Subscribe("transactions");
+
+            var cancellationTokenSource = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) =>
+            {
+                e.Cancel = true;
+                cancellationTokenSource.Cancel();
+            };
+
+            try
+            {
+                while (true)
+                {
+                    var consumeResult = consumer.Consume(cancellationTokenSource.Token);
+                    Console.WriteLine($"Received message: {consumeResult.Message.Value}");
+                }
+            }
+            catch (OperationCanceledException)
+            {
+                consumer.Close();
+            }
+        }
+    }
+}
---
+++ KafkaProducer/Program.cs
@@ -0,0 +1,32 @@
+﻿using Confluent.Kafka;
+using System;
+using System.Threading.Tasks;
+
+class Program
+{
+    public static async Task Main(string[] args)
+    {
+        var config = new ProducerConfig
+        {
+            BootstrapServers = "localhost:9092"
+        };
+
+        using (var producer = new ProducerBuilder<Null, string>(config).Build())
+        {
+            try
+            {
+
+                var guid=Guid.NewGuid().ToString();
+
+                var message = $"TransactionID: {guid} | Amount: $100 | AccountFrom: 123 | AccountTo: 456";
+                var deliveryReport = await producer.ProduceAsync("transactions", new Message<Null, string> { Value = message });
+
+                Console.WriteLine($"Message '{deliveryReport.Value}' delivered to '{deliveryReport.TopicPartitionOffset}'");
+            }
+            catch (ProduceException<Null, string> ex)
+            {
+                Console.WriteLine($"Error: {ex.Error.Reason}");
+            }
+        }
+    }
+}
